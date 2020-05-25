84354f7676858ff305e1feb6dcd3661a9fd1b787
Sakshi Gupta <itzsakshi26gupta@gmail.com>
2020-05-25 10:07:14 +0530

    Second Commit

--- src/main/java/com/javainuse/controller/JwtAuthenticationController.java
+++ src/main/java/com/javainuse/controller/JwtAuthenticationController.java
@@ -1,7 +1,5 @@
 package com.javainuse.controller;
 
-//import java.util.Objects;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
@@ -14,12 +12,12 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
-import com.javainuse.service.JwtUserDetailsService;
-
 
 import com.javainuse.config.JwtTokenUtil;
 import com.javainuse.model.JwtRequest;
 import com.javainuse.model.JwtResponse;
+import com.javainuse.model.UserDTO;
+import com.javainuse.service.JwtUserDetailsService;
 
 @RestController
 @CrossOrigin
@@ -39,12 +37,16 @@ public class JwtAuthenticationController {
 
 		authenticate(authenticationRequest.getUsername(), authenticationRequest.getPassword());
 
-		final UserDetails userDetails = userDetailsService
-				.loadUserByUsername(authenticationRequest.getUsername());
+		final UserDetails userDetails = userDetailsService.loadUserByUsername(authenticationRequest.getUsername());
 
 		final String token = jwtTokenUtil.generateToken(userDetails);
 
 		return ResponseEntity.ok(new JwtResponse(token));
+	}
+
+	@RequestMapping(value = "/register", method = RequestMethod.POST)
+	public ResponseEntity<?> saveUser(@RequestBody UserDTO user) throws Exception {
+		return ResponseEntity.ok(userDetailsService.save(user));
 	}
 
 	private void authenticate(String username, String password) throws Exception {
--- src/main/java/com/javainuse/service/JwtUserDetailsService.java
+++ src/main/java/com/javainuse/service/JwtUserDetailsService.java
@@ -2,22 +2,41 @@ package com.javainuse.service;
 
 import java.util.ArrayList;
 
-import org.springframework.security.core.userdetails.User;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+
+import com.javainuse.dao.UserDao;
+import com.javainuse.model.DAOUser;
+import com.javainuse.model.UserDTO;
 
 @Service
 public class JwtUserDetailsService implements UserDetailsService {
 
+	@Autowired
+	private UserDao userDao;
+
+	@Autowired
+	private PasswordEncoder bcryptEncoder;
+
 	@Override
 	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-		if ("javainuse".equals(username)) {
-			return new User("javainuse", "$2a$10$slYQmyNdGzTn7ZLBXBChFOC9f6kFjAqPhccnP6DxlWXx2lPk1C3G6",
-					new ArrayList<>());
-		} else {
+
+		DAOUser user = userDao.findByUsername(username);
+		if (user == null) {
 			throw new UsernameNotFoundException("User not found with username: " + username);
 		}
+		return new org.springframework.security.core.userdetails.User(user.getUsername(), user.getPassword(),
+				new ArrayList<>());
+	}
+
+	public DAOUser save(UserDTO user) {
+		DAOUser newUser = new DAOUser();
+		newUser.setUsername(user.getUsername());
+		newUser.setPassword(bcryptEncoder.encode(user.getPassword()));
+		return userDao.save(newUser);
 	}
 }
